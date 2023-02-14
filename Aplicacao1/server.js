1725d31edbf79513b570e5f97ff2f790a69b503d
VitorBCC007 <118393027+VitorBCC007@users.noreply.github.com>
2023-02-14 15:07:34 -0300

    Create server.js

---
+++ Aplicacao1/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const redis = require('redis');
+const app = express();
+const redisClient = redis.createClient({
+    host: 'test-redis',
+    port: 6379
+});
+
+app.get('/', function(req, res) {
+    redisClient.get('numVisits', function(err, numVisits){
+        numVisitsToDisplay = parseInt(numVisits)+1;
+        if(isNaN(numVisitsToDisplay)){
+            numVisitsToDisplay = 1;
+        }
+        res.send('Aplicacao-1: Número de visitantes é: '+ numVisitsToDisplay);
+        numVisits++;
+        redisClient.set('numVisits', numVisits);
+    });
+});
+
+app.listen(5000, function(){
+    console.log('Web app está escutando porta 5000');
+});
